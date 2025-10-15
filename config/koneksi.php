7987a6c43b08123c32d8a32ad2911561e40435c5
FERDINAN JUFRI MALEKE <117708688+jufrimaleke@users.noreply.github.com>
2025-10-15 12:19:42 +0800

    Delete config directory

--- config/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-function koneksi_db() {
-    $conn = mysqli_connect("localhost", "root", "", "db_kampus");
-    if (!$conn) {
-        die("Koneksi gagal: " . mysqli_connect_error());
-    }
-    return $conn;
-}
-?>
